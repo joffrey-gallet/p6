4d0370996d55104d3aeff578d25dfc7f31c7998c
Joffrey Gallet <joffrey.gallet@gmail.com>
2023-01-20 09:54:10 +0100

    connection mongo et mise en place des parcours sauce et users

--- app.js
+++ app.js
@@ -2,10 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.use((request, response, next) => {
-    console.log();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://joffreyG:2512AedJM2962013@cluster0.zgid3wg.mongodb.net/test?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
+
+
 app.use((reqest, response, next) => {
     response.json({ message: 'Request is ok !' });
 });
